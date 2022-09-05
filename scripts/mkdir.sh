88a9b302787d6e0a02708f0fce192e807de59bc0
akshay-roxiler <96765800+akshay-roxiler@users.noreply.github.com>
2022-09-05 09:12:51 +0530

    added new cmds

--- scripts/mkdir.sh
+++ scripts/mkdir.sh
@@ -1,5 +1,6 @@
 mkdir -p /web
 curl -O https://bootstrap.pypa.io/get-pip.py
-python3.7 get-pip.py --user
 python -m pip --version
-python3.7 pip install Flask
+python -m pip install requests
+python get-pip.py --user
+python pip install Flask
