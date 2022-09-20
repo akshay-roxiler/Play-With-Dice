fa8d346692b8775e6647666b01b45ca461154df3
akshay@roxiler <akshay.mankar@roxiler.com>
2022-09-20 13:57:03 +0530

    Added new project

--- scripts/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -x
-APP_HOME="/home/ec2-user/server2"
-ifAppRunning=`pgrep java`
-if [[ -n  $ifAppRunning ]]; then
-   sudo killall java 
-fi
-echo "listing files..."
-ls -l $APP_HOME/
